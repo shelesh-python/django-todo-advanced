57227afea88f1694dc7922f79fb5c14033127554
shelesh-python <sheleshc87@gmail.com>
2026-01-09 02:45:51 +0530

    Auto create admin for Render free plan

--- tasks/apps.py
+++ tasks/apps.py
@@ -1,5 +1,23 @@
 from django.apps import AppConfig
+from django.contrib.auth.models import User
+import os
 
 
 class TasksConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'tasks'
+
+    def ready(self):
+        # Auto-create superuser on Render (Free plan workaround)
+        username = os.getenv("shelesh")
+        password = os.getenv("123shelesh##")
+        email = os.getenv("sheleshc87@gmail.com")
+
+        if username and password:
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(
+                    username=username,
+                    password=password,
+                    email=email
+                )
+                print("✅ Admin user created automatically")
--- todo_project/settings.py
+++ todo_project/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'tasks',
+    'tasks.apps.TasksConfig',
+
 
 ]
 
